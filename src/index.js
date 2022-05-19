d3780234fee7006d7dc637d658615acf588c81cc
humberto2903 <105809384+humberto2903@users.noreply.github.com>
2022-05-19 09:12:06 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//settings
+app.set('port',4000);
+app.set('view engine','ejs'); 
+
+//middlewares
+
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'views/Sinaloa.html'))
+});
+
+//static files
+
+//feedback
+app.listen(app.get('port')),() => {
+    console.log('Server on port',app.get('port'));
+}
